7a4d313242b682d7b3f8ddc2cfb86d3b1b70d23f
atulthainuan157 <atulthainuan157@gmail.com>
2026-06-01 01:32:18 +0530

    chore: remove redundant code changes and clean up repository

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const editorRef = useRef(null)
+
+  const ydoc = useMemo(() => new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
+
+  const handleMount = (editor) => {
+    editorRef.current = editor
+
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect: true
+    })
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+
+    
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg ">
+      </aside>
+      <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+      <Editor
+        height="100%"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        theme="vs-dark"
+        onMount={handleMount}
+      />
+      </section>
+    </main>
   )
 }
 
