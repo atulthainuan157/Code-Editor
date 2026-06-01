f3ebcbf60690b3d48c4d4baa1d117dc6fd68bdd6
atulthainuan157 <atulthainuan157@gmail.com>
2026-06-01 22:48:01 +0530

    feat: add username input and handling to the editor component

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css"
 import { Editor } from "@monaco-editor/react"
 import { MonacoBinding } from "y-monaco"
-import { useRef, useMemo } from "react"
+import { useRef, useMemo, useState } from "react"
 import * as Y from "yjs"
 import { SocketIOProvider } from "y-socket.io"
 
 function App() {
 
   const editorRef = useRef(null)
+  const [username, setUsername] = useState("")
 
   const ydoc = useMemo(() => new Y.Doc(), [])
   const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
@@ -26,6 +27,32 @@ function App() {
     )
 
     
+  }
+
+  const handleJoin = (e) => {
+    e.preventDefault()
+    setUsername(e.target.username.value)
+    window.history.pushState({}, "", "?username=" + e.target.username.value)
+  }
+
+  if(!username) {
+    return (
+      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center">
+        <form onSubmit={handleJoin} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Enter your username"
+            className="p-2 rounded-lg bg-gray-800 text-white"
+            name="username"
+            />
+            <button
+              className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+            >
+              join
+          </button>
+        </form>
+      </main>
+    )
   }
 
 
